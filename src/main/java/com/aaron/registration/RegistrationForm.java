9035ffbab4a39c020a835789bdf2c6b208ed1365
Aarón Espí <aaronespi@MacBook-Pro-de-Aaron.local>
2025-06-16 12:27:42 +0200

    improve registration form wait

--- src/main/java/com/aaron/registration/RegistrationForm.java
+++ src/main/java/com/aaron/registration/RegistrationForm.java
@@ -100,7 +100,8 @@ public class RegistrationForm extends AbstractComponent {
 
     @Override
     public boolean isDisplayed() {
-        return this.wait.until((d) -> firstName.isDisplayed() && save.isDisplayed() && termsAndPrivacy.isDisplayed());
+        return this.wait.until(
+                (d) -> firstName.isDisplayed() && email.isDisplayed() && password.isDisplayed() && save.isDisplayed());
     }
 
 }
