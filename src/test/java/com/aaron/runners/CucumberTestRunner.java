7f2c3d60c286e96b46247736ef890a4b9baf483d
Aarón Espí <aaronespi@MacBook-Pro-de-Aaron.local>
2025-06-21 19:47:19 +0200

    allure report configured

--- src/test/java/com/aaron/runners/CucumberTestRunner.java
+++ src/test/java/com/aaron/runners/CucumberTestRunner.java
@@ -14,9 +14,10 @@ import io.cucumber.junit.CucumberOptions;
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", glue = { "com.aaron.steps", "com.aaron.hooks" }, plugin = {
-                "pretty",
-                "html:target/cucumber-reports.html",
-                "json:target/cucumber-report.json"
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+        "pretty",
+        "html:target/cucumber-reports.html",
+        "json:target/cucumber-report.json"
 }, monochrome = true)
 public class CucumberTestRunner {
 }
